84050b118fa0da2ad3f4f3a2f758bd2813cb8f09
halynaburdiian <verdamtion@gmail.com>
2019-07-11 16:40:37 +0200

    get pokemons by rarity

--- 2.reduce.js
+++ 2.reduce.js
@@ -23,15 +23,47 @@ const calculateTotalEggDistance = (pokemons)=>{
     }, 0)
 }
 
-const getHeaviestPokemon = (pokemons) =>{
-    const heaviest = pokemons.find(pokemon => pokemon.weight === '460.0 kg');
-    return heaviest.id
-}
+const getHeaviestPokemon = (pokemons) => pokemons.reduce(function (current, pokemon) {
+    if (parseFloat(current.weight) < parseFloat(pokemon.weight)) {
+       return pokemon;
+    } else {
+        return current;
+    }
+   })
+    // const heaviest = pokemons.find(pokemon => pokemon.weight === '460.0 kg');
+    // return heaviest
+    
 
+
+const categorizePokemonsByRarity =(pokemons) =>{
+    const pokemonsByRarity ={
+        common : [],
+        rare : [],
+        legendary : []
+    }
+
+    const pokemonsByRarityClasses = pokemons.reduce(function(pokemon,current){
+        if (pokemon.spawn_chance > 0.1){
+            return current.common.push(pokemon)
+        }
+        else if (pokemon.spawn_chance < 0.1, pokemon.spawn_chance > 0.01){
+            return current.rare.push(pokemon)
+        }
+        else return current
+    }, pokemonsByRarity )
+
+    
+return pokemonsByRarityClasses
+
+}
+// common has spawn_chance above 0.1
+// rare has spawn_chance below 0.1 but higher than 0.01
+// legendary has a spawn chance of 0.01 or lower
 module.exports = {
     calculateTotalPokemonWeight,
     calculateAverageSpawnChance,
     calculateTotalEggDistance,
     getHeaviestPokemon,
+    categorizePokemonsByRarity
 }
 
